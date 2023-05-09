c497a2af75e8587007c3c0033e6b46ff00ae4434
betoanh <nguyenbetoanh1999@gmail.com>
2023-05-09 15:46:46 +0700

    lan1

---
+++ app/Models/ClassIT.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassIT extends Model
+{
+    use HasFactory;
+    protected $table ='class_its';
+    protected $primaryKey='classId';
+    public $timestamps=false;
+    
+     public function teacher()
+    {
+        return $this->hasOne(Teacher::class,'class_Id','classId');
+    }
+    public function schoolFun(){
+        return $this->belongsTo(School::class,'school_Id','schoolId');
+    }
+    
+}
